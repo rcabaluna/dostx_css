072bc4af9120356d530a986cf0055df6008ff32b
Ruel Cabaluna Jr <rcabalunajr@gmail.com>
2024-08-02 16:36:31 +0800

    adjusted servicetypes

--- application/views/survey/survey-form.php
+++ application/views/survey/survey-form.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <title>Enlink - Admin Dashboard Template</title>
+        <title>DOSTX - <?=strtoupper($surveytype)?> Survey CSS</title>
 
         <!-- Favicon -->
         <link rel="shortcut icon" href="<?=base_url('assets/images/logo/favicon.png')?>">
